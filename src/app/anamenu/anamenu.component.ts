1067dc30a762acb21e79d9f7f9a1a1dccf62b091
Egemen Karaman <egemenkaraman7161@gmail.com>
2024-09-10 17:31:36 +0300

    ANAMENÜYE BUTONLAR KONULDU VE YÖNLENDİRMELER YAPILDI

--- src/app/anamenu/anamenu.component.ts
+++ src/app/anamenu/anamenu.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { HastaneService } from '../hastane.service';
+import { Route } from '@angular/router';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-anamenu',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,  HttpClientModule, CommonModule,HttpClientJsonpModule],
   templateUrl: './anamenu.component.html',
   styleUrl: './anamenu.component.css'
 })
 export class AnamenuComponent {
+
+  constructor(private hastaneService: HastaneService, private router: Router) {}
+
+  goListe(){
+    this.router.navigate(['/liste']);
+  }
+  goHastaSil(){
+    this.router.navigate(['/hasta-sil']);
+  }
+  goHastaEkle(){
+    this.router.navigate(['/hasta-ekle']);
+  }
+  logout() {
+    this.router.navigate(['']);
+  }
+
 
 }
