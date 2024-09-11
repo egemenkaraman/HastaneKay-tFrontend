39c216355c3c2cf30561f0950f14c9afd726be07
Egemen Karaman <egemenkaraman7161@gmail.com>
2024-09-11 15:29:56 +0300

    HASTA SİLME VE LOGİN CSS YAPILDI

--- src/app/hasta-sil/hasta-sil.component.ts
+++ src/app/hasta-sil/hasta-sil.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { error } from 'console';
+import { HastaneService } from '../hastane.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule,Router } from '@angular/router';
 
 @Component({
   selector: 'app-hasta-sil',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,  HttpClientModule, CommonModule, RouterModule,ReactiveFormsModule],
   templateUrl: './hasta-sil.component.html',
   styleUrl: './hasta-sil.component.css'
 })
 export class HastaSilComponent {
+  id: string = '';
+  message: string = '';
+  
+  constructor(private hastaneService: HastaneService, private router: Router) {}
+
+  deleteHasta(){
+    this.hastaneService.deleteHasta(this.id).subscribe({
+      next: (response) => this.message = response,
+      error: (err) => this.message = 'Hasta Başarıyla Silindi.'
+    });
+  }
+  goBack() {
+    this.router.navigate(['/anamenü']);
+  }
 
 }
